1745c3faeb499689677d26d1f5c7c46aa987003e
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2009-10-26 00:35:23 +0100

    updated aliases

---
+++ aliases/host_aliases/host_aliases.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require "yaml"
+
+begin
+  host_aliases = 
+    YAML::load_file(File.expand_path(File.join(File.dirname(__FILE__), "host_aliases.yml")))
+rescue Errno::ENOENT
+  exit
+end
+
+exit unless host_aliases.is_a?(Hash)
+
+hosts = []
+
+host_aliases.each_key do |protocol|
+  host_aliases[protocol].each_key do |alias_host|
+    host = host_aliases[protocol][alias_host]
+    hosts << %(alias #{protocol}-#{alias_host}="expect -c 'spawn #{protocol} #{host[:username]}@#{host[:host]}; expect assword: ; send #{'\"' << host[:password] << '\n\"' } ; interact'")
+  end
+end
+
+puts hosts.join(";")
