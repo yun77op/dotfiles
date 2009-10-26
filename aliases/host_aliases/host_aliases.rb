13070b41ac25d295e4a39f4f7ff78a52208aaa5c
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2009-10-26 18:17:57 +0100

    fixing aliases

--- aliases/host_aliases/host_aliases.rb
+++ aliases/host_aliases/host_aliases.rb
@@ -16,7 +16,7 @@ hosts = []
 host_aliases.each_key do |protocol|
   host_aliases[protocol].each_key do |alias_host|
     host = host_aliases[protocol][alias_host]
-    hosts << %(alias #{protocol}-#{alias_host}="expect -c 'spawn #{protocol} #{host[:username]}@#{host[:host]}; expect assword: ; send #{'\"' << host[:password] << '\n\"' } ; interact'")
+    hosts << %(alias #{protocol}-#{alias_host}="expect -c 'spawn #{protocol} #{host[:username].to_s}@#{host[:host].to_s}; expect assword: ; send #{'\"' << host[:password].to_s << '\n\"' } ; interact'")
   end
 end
 
