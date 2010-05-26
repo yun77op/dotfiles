85c966f40320032c34f3ee9d4ddfca1f992f3516
NEY Jean-Sébastien <jeansebastien.ney@gmail.com>
2010-05-26 10:03:26 +0200

    ftp normal way

--- aliases/host_aliases/host_aliases.rb
+++ aliases/host_aliases/host_aliases.rb
@@ -16,7 +16,11 @@ hosts = []
 host_aliases.each_key do |protocol|
   host_aliases[protocol].each_key do |alias_host|
     host = host_aliases[protocol][alias_host]
-    hosts << %(alias #{protocol}-#{alias_host}="expect -c 'spawn #{protocol} #{host[:username].to_s}@#{host[:host].to_s}; expect assword: ; send #{'\"' << host[:password].to_s << '\n\"' } ; interact'")
+    if protocol.to_s == "ftp"
+      hosts << %(alias #{protocol}-#{alias_host}="ftp #{host[:username].to_s}:#{host[:password].to_s}@#{host[:host].to_s}")
+    else
+      hosts << %(alias #{protocol}-#{alias_host}="expect -c 'spawn #{protocol} #{host[:username].to_s}@#{host[:host].to_s}; expect assword: ; send #{'\"' << host[:password].to_s << '\n\"' } ; interact'")
+    end
   end
 end
 
